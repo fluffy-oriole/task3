57c6b02933c2300f7816b9c8c92a039743b6782e
Flffy-Oriole <aarrtteemm2006@gmail.com>
2026-02-20 02:02:58 +0800

    Добавьте файлы проекта.

---
+++ task3/Program.cs
@@ -0,0 +1,56 @@
+﻿namespace task3
+{
+    public class Logic
+    {
+        public static string IsCharsInWord(string firstWord, string secondWord)
+        {
+            List<char> alredyUsedChars = new List<char>();
+            string answer = "";
+            for (int i = 0; i < firstWord.Length; i++)
+            {
+                char ch = firstWord[i];
+                bool isCharAlredyUsed = false;
+                for (int k = 0; k < alredyUsedChars.Count; k++)
+                {
+                    if (alredyUsedChars[k] == ch)
+                    {
+                        isCharAlredyUsed = true;
+                        break;
+                    }
+                }
+
+                if (!isCharAlredyUsed)
+                {
+                    bool isCharPresentInWord = false;
+                    alredyUsedChars.Add(ch);
+                    for (int j = 0; j < secondWord.Length; j++)
+                    {
+                        if (ch == secondWord[j])
+                        {
+                            isCharPresentInWord = true;
+                            answer += "да ";
+                            break;
+                        }
+                    }
+                    if (!isCharPresentInWord)
+                    {
+                        answer += "нет ";
+                    }
+                }
+            }
+            return answer;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите первое слово: ");
+            string firstWord = Console.ReadLine();
+            Console.Write("Введите второе слово: ");
+            string secondWord = Console.ReadLine();
+            Console.WriteLine(Logic.IsCharsInWord(firstWord, secondWord));
+
+        }
+    }
+}
