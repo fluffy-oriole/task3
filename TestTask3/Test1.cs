9f32de77f26fff5427cf50809b657628e5c16602
Flffy-Oriole <aarrtteemm2006@gmail.com>
2026-02-20 21:11:51 +0800

    добавлены тесты

---
+++ TestTask3/Test1.cs
@@ -0,0 +1,43 @@
+﻿using task3;
+
+namespace TestTask3
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void RegularInput()
+        {
+            string answer = Logic.IsCharsInWord("процессор", "информация");
+            Assert.AreEqual("нет да да да нет нет ", answer);
+        }
+
+        [TestMethod]
+        public void NoCharsInWord()
+        {
+            string answer = Logic.IsCharsInWord("hello", "zdravstvuy");
+            Assert.AreEqual("нет нет нет нет ", answer);
+        }
+
+        [TestMethod]
+        public void EveryCharInWord()
+        {
+            string answer = Logic.IsCharsInWord("hello", "olleh");
+            Assert.AreEqual("да да да да ", answer);
+        }
+
+        [TestMethod]
+        public void NoCharsInFirstWord()
+        {
+            string answer = Logic.IsCharsInWord("", "hello");
+            Assert.AreEqual("", answer);
+        }
+
+        [TestMethod]
+        public void NoCharsInSecondWord()
+        {
+            string answer = Logic.IsCharsInWord("hello", "");
+            Assert.AreEqual("нет нет нет нет ", answer);
+        }
+    }
+}
